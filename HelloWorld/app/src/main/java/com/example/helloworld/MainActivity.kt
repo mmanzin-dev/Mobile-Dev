faec021e5653f37c2ad7604b642946d28367efa6
Matko Manzin <matko.manzin.zg@gmail.com>
2025-10-31 18:52:01 +0100

    Simple TextView change using a button

---
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+private lateinit var txtView: TextView
+private lateinit var button: Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val txt = findViewById<TextView>(R.id.textView)
+        val btn = findViewById<Button>(R.id.btnChangeText)
+
+        btn.setOnClickListener {
+            txt.text = "The world has changed"
+        }
+    }
+}
